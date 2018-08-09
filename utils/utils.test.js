16cdbd6e7232739e7f3b98f0a1e21a7956729b06
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-10 03:57:20 +0800

    Using an Assertion Library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,40 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`);
-  }
+  expect(res).toBe(9).toBeA('number');
 });
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+it('should set firstName and lastName', () => {
+  var user = {location: 'Hong Kong', age: 45};
+  var res = utils.setName(user, 'Robert Lowe');
+  
+  expect(res).toInclude({
+    firstName: 'Robert',
+    lastName: 'Lowe'
+  }).toBeA('object');
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(12);
+//   // expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+//   // expect([2,3,4]).toExclude(1);
+//   expect({
+//     name: 'Robert',
+//     age: 45,
+//     location: 'Hong Kong'
+//   }).toExclude({
+//     age: 43
+//   })
+// });
