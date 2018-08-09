ac2a0471c77894a206a286a226aba4112207efd9
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-10 05:31:35 +0800

    Organizing Test With describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,30 +3,40 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
+// Server
+  // GET /
+    // some test case
+  // GET /users
+    // some test case
 
-// Make a new test
-// assert 200
-// Assert that you exist in users array
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Robert',
-        age: 45
-      });
-    })
-    .end(done);
+describe('Server', () => {
+
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', () => {
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Robert',
+            age: 45
+          });
+        })
+        .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,31 +2,39 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+describe('Utils', () => {
 
-  expect(res).toBe(44).toBeA('number');
-});
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+      expect(res).toBe(44).toBeA('number');
+    });
+  })
+
+
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    });
+  });
+
+  it('should square a number', () => {
+    var res = utils.square(5);
+
+    expect(res).toBe(25).toBeA('number');
+  });
+
+  it('should async square a number', (done) => {
+    utils.asyncSquare(3, (res) => {
+      expect(res).toBe(9).toBeA('number');
+      done();
+    });
   });
 });
 
-it('should square a number', () => {
-  var res = utils.square(5);
 
-  expect(res).toBe(25).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(3, (res) => {
-    expect(res).toBe(9).toBeA('number');
-    done();
-  });
-});
 
 // should verify first and last names are set
 // assert it includes firstName and lastName with proper values
