c96df239e4a512e33dd16edd073131f5c928e1eb
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-10 05:10:55 +0800

    Testing Express Applications: Part II

--- server/server.js
+++ server/server.js
@@ -3,7 +3,26 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
 });
 
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Mike',
+    age: 27
+  }, {
+    name: 'Robert',
+    age: 45
+  }, {
+    name: 'Jen',
+    age: 26
+  }]);
+});
+// GET /users
+// Give users a name prop and age prop
+
 app.listen(3000);
+module.exports.app = app;
